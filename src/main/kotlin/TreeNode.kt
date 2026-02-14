65b767646de0a1d4afeb73b2fc396c9654da5a55
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-14 19:32:36 +0100

    feature(main): provide level order traversal

--- src/main/kotlin/TreeNode.kt
+++ src/main/kotlin/TreeNode.kt
@@ -9,4 +9,20 @@ class TreeNode<T>(val value: T) {
         visit(this)
         children.forEach { it.forEachDepthFirst(visit) }
     }
+
+    fun forEachLevelOrder(visit: Visitor<T>) {
+        visit(this)
+
+        //root node
+        val queue = ArrayListQueue<TreeNode<T>>()
+        children.forEach { queue.enqueue(it) }
+
+        //children nodes
+        var node = queue.dequeue()
+        while (node != null) {
+            visit(node)
+            node.children.forEach { queue.enqueue(it) }
+            node = queue.dequeue()
+        }
+    }
 }
