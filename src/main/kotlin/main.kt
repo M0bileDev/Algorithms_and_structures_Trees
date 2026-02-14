baa5b5e8676fbb39a852622de864fad1e10de346
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-14 18:59:27 +0100

    feature(main): create tree and test depth first traversal

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,13 @@
 fun main() {
 
+    makeAppleTree()
+    val beverageTree = makeBeverageTree()
+    beverageTree.forEachDepthFirst {
+        println(it.value)
+    }
+}
+
+fun makeAppleTree() {
     val green = TreeNode("Green")
     val red = TreeNode("Red")
 
@@ -7,4 +15,44 @@ fun main() {
         add(green)
         add(red)
     }
+}
+
+fun makeBeverageTree(): TreeNode<String> {
+    val tree = TreeNode("Beverages")
+
+    val hot = TreeNode("hot")
+    val cold = TreeNode("cold")
+
+    val tea = TreeNode("tea")
+    val coffee = TreeNode("coffee")
+    val chocolate = TreeNode("cocoa")
+
+    val blackTea = TreeNode("black")
+    val greenTea = TreeNode("green")
+    val chaiTea = TreeNode("chai")
+
+    val soda = TreeNode("soda")
+    val milk = TreeNode("milk")
+
+    val gingerAle = TreeNode("ginger ale")
+    val bitterLemon = TreeNode("bitter lemon")
+
+    tree.add(hot)
+    tree.add(cold)
+
+    hot.add(tea)
+    hot.add(coffee)
+    hot.add(chocolate)
+
+    cold.add(soda)
+    cold.add(milk)
+
+    tea.add(blackTea)
+    tea.add(greenTea)
+    tea.add(chaiTea)
+
+    soda.add(gingerAle)
+    soda.add(bitterLemon)
+
+    return tree
 }
